708f8b349baeb8d3fdf568a7db4517ef423a91f0
YuTsaiProject <mark970157@gmail.com>
2017-06-22 18:52:06 +0800

    GIF

---
+++ app/src/main/java/com/myandroid/androidgiftest/MainActivity.java
@@ -0,0 +1,31 @@
+package com.myandroid.androidgiftest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.androidgif.R;
+
+public class MainActivity extends Activity {
+
+    TextView textViewInfo;
+    GifView gifView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        gifView = (GifView)findViewById(R.id.gifview);
+        textViewInfo = (TextView)findViewById(R.id.textinfo);
+
+        String stringInfo = "";
+        stringInfo += "Duration: " + gifView.getMovieDuration() + "\n";
+        stringInfo += "W x H: "
+                + gifView.getMovieWidth() + " x "
+                + gifView.getMovieHeight() + "\n";
+
+        textViewInfo.setText(stringInfo);
+
+    }
+
+}
